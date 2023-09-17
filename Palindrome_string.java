21d3ad49c83ad5317240b4405fb29e0866cc97df
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2023-09-17 15:05:43 +0530

    Update and rename palin.java to Palindrome_string.java

--- Palindrome_string.java
+++ Palindrome_string.java
@@ -5,7 +5,7 @@
 
 import java.util.*;
 
-class palin {
+class Palindrome_string {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
